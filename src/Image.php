72aefbf667c0453763c9f6c1e121c1cc68de40b8
Carson Full <carson.full@gmomail.org>
2015-09-29 19:39:20 -0500

    [Image] Added magic call to pass through to Image\Info before plugins. This is for BC.

--- src/Image.php
+++ src/Image.php
@@ -2,6 +2,11 @@
 
 namespace Bolt\Filesystem;
 
+/**
+ * An image file.
+ *
+ * @author Carson Full <carsonfull@gmail.com>
+ */
 class Image extends File
 {
     /** @var Image\Info */
@@ -23,5 +28,25 @@ class Image extends File
             $this->info = $this->filesystem->getImageInfo($this->path);
         }
         return $this->info;
+    }
+
+    /**
+     * Pass-through to Image\Info, then plugins. This is for BC.
+     *
+     * @param string $method
+     * @param array  $arguments
+     *
+     * @return mixed
+     */
+    public function __call($method, array $arguments)
+    {
+        $info = $this->getInfo();
+        if (method_exists($info, 'get' . $method)) {
+            return call_user_func([$info, 'get' . $method]);
+        } elseif (method_exists($info, 'is' . $method)) {
+            return call_user_func([$info, 'is' . $method]);
+        } else {
+            return parent::__call($method, $arguments);
+        }
     }
 }
