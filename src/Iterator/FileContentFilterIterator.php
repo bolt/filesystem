57b9f28b46846ce4f9cbcddf9c28ee6c41594756
Carson Full <carson.full@gmomail.org>
2015-11-21 15:04:28 -0600

    [Iterators] Implemented FileContentFilterIterator

---
+++ src/Iterator/FileContentFilterIterator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Bolt\Filesystem\Iterator;
+
+use Bolt\Filesystem\Exception\IOException;
+use Bolt\Filesystem\File;
+use Symfony\Component\Finder\Iterator\FilecontentFilterIterator as FilecontentFilterIteratorBase;
+
+/**
+ * {@inheritdoc}
+ *
+ * @author Carson Full <carsonfull@gmail.com>
+ */
+class FileContentFilterIterator extends FilecontentFilterIteratorBase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function accept()
+    {
+        if (!$this->matchRegexps && !$this->noMatchRegexps) {
+            return true;
+        }
+
+        /** @var File $handler */
+        $handler = $this->current();
+
+        if (!$handler->isFile()) {
+            return false;
+        }
+
+        try {
+            $content = $handler->read();
+        } catch (IOException $e) {
+            return false;
+        }
+
+        // should at least not matach one rule to exclude
+        foreach ($this->noMatchRegexps as $regex) {
+            if (preg_match($regex, $content)) {
+                return false;
+            }
+        }
+
+        // should at least match one rule
+        $match = true;
+        if ($this->matchRegexps) {
+            $match = false;
+            foreach ($this->matchRegexps as $regex) {
+                if (preg_match($regex, $content)) {
+                    return true;
+                }
+            }
+        }
+
+        return $match;
+    }
+}
