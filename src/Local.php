d610baf513b313af63f8af72c1e41c7e7594a4af
Carson Full <carson.full@gmomail.org>
2015-11-24 23:26:17 -0600

    [Local] Throwing new DirectoryCreationException when creating a directory fails instead of a false return

--- src/Local.php
+++ src/Local.php
@@ -2,6 +2,7 @@
 
 namespace Bolt\Filesystem;
 
+use Bolt\Filesystem\Exception\DirectoryCreationException;
 use League\Flysystem\Adapter\Local as LocalBase;
 use League\Flysystem\Config;
 use League\Flysystem\Util;
@@ -19,37 +20,11 @@ class Local extends LocalBase
             umask($umask);
 
             if (!$result) {
-                return false;
+                throw new DirectoryCreationException($root);
             }
         }
 
         return realpath($root);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function write($path, $contents, Config $config)
-    {
-        $location = $this->applyPathPrefix($path);
-        if (!$this->ensureDirectory(dirname($location))) {
-            return false;
-        }
-
-        return parent::write($path, $contents, $config);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function writeStream($path, $resource, Config $config)
-    {
-        $location = $this->applyPathPrefix($path);
-        if (!$this->ensureDirectory(dirname($location))) {
-            return false;
-        }
-
-        return parent::writeStream($path, $resource, $config);
     }
 
     /**
@@ -69,35 +44,6 @@ class Local extends LocalBase
         }
 
         return compact('path', 'size', 'contents', 'mimetype');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function rename($path, $newpath)
-    {
-        $location = $this->applyPathPrefix($path);
-        $destination = $this->applyPathPrefix($newpath);
-        $parentDirectory = $this->applyPathPrefix(Util::dirname($newpath));
-        if (!$this->ensureDirectory($parentDirectory)) {
-            return false;
-        }
-
-        return rename($location, $destination);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function copy($path, $newpath)
-    {
-        $location = $this->applyPathPrefix($path);
-        $destination = $this->applyPathPrefix($newpath);
-        if (!$this->ensureDirectory(dirname($destination))) {
-            return false;
-        }
-
-        return copy($location, $destination);
     }
 
     /**
